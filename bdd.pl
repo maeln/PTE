c62879ad76218e21dc42e58e608f0c36f956d890
maeln <contact@maeln.com>
2013-04-01 18:17:13 +0200

    Remove useless file.

--- bdd.pl
+++
@@ -1,7 +0,0 @@
-reduireFosse(E, G) :- atomic_list_concat(F,' ', E), smstofre(F, G).
-
-:-[dico].
-
-smstofre([], []).
-smstofre([T|R], [X|Y]) :- frword(T,X), smstofre(R,Y).
-
--- dico.pl
+++
@@ -1,5 +0,0 @@
-frword(bjr,bonjour).
-frword(slt,salut).
-frword(cb,combien).
-frword(mdr,'mort de rire').
-frword(E,E).
